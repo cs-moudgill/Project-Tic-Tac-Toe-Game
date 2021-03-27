b012c1cd66f1120a41929c067ee0ba36a5574d6a
cs-moudgill <channpreet.moudgill@gmail.com>
2021-03-27 00:04:45 -0300

    bug fixes

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -10,7 +10,7 @@ if(!selectedSquares.some(element=>element.includes(squareNumber))){
     select.style.backgroundImage="url('images/o.png')"
    }
    selectedSquares.push(squareNumber+activePlayer);
-   checkWinCondition();
+   checkWinConditions();
    if(activePlayer==='X'){
        activePlayer='O';
    } else{
@@ -19,7 +19,7 @@ if(!selectedSquares.some(element=>element.includes(squareNumber))){
    audio('./media/place.mp3');
    if(activePlayer==='O'){
        disableClick();
-       setTimeout(() => {
+       setTimeout(function() {
            computersTurn()
        }, 1000);
    }
@@ -39,7 +39,7 @@ while(!success){
 }
 }
 
-function checkWinCondition(){
+function checkWinConditions(){
 if(arrayIncludes('0X','1X','2X')){drawWinLine(50,100,558,100);}
 else if(arrayIncludes('3X','4X','5X')){drawWinLine(50,304,558,304);}
 else if(arrayIncludes('6X','7X','8X')){drawWinLine(50,508,558,508);}
@@ -48,17 +48,17 @@ else if(arrayIncludes('1X','4X','7X')){drawWinLine(304,50,304,558);}
 else if(arrayIncludes('2X','5X','8X')){drawWinLine(508,50,508,558);}
 else if(arrayIncludes('6X','4X','2X')){drawWinLine(100,508,510,90);}
 else if(arrayIncludes('0X','4X','8X')){drawWinLine(100,100,520,520);}
-else if(arrayIncludes('0o','1o','2o')){drawWinLine(50,100,558,100);}
-else if(arrayIncludes('3o','4o','5o')){drawWinLine(50,304,558,304);}
-else if(arrayIncludes('6o','7o','8o')){drawWinLine(50,508,558,508);}
-else if(arrayIncludes('0o','3o','6o')){drawWinLine(100,50,100,558);}
-else if(arrayIncludes('1o','4o','7o')){drawWinLine(304,50,304,558);}
-else if(arrayIncludes('2o','5o','8o')){drawWinLine(508,50,508,558);}
-else if(arrayIncludes('6o','4o','2o')){drawWinLine(100,508,510,90);}
-else if(arrayIncludes('0o','4o','8o')){drawWinLine(100,100,520,520);}
+else if(arrayIncludes('0O','1O','2O')){drawWinLine(50,100,558,100);}
+else if(arrayIncludes('3O','4O','5O')){drawWinLine(50,304,558,304);}
+else if(arrayIncludes('6O','7o','8o')){drawWinLine(50,508,558,508);}
+else if(arrayIncludes('0O','3O','6O')){drawWinLine(100,50,100,558);}
+else if(arrayIncludes('1O','4O','7O')){drawWinLine(304,50,304,558);}
+else if(arrayIncludes('2O','5O','8O')){drawWinLine(508,50,508,558);}
+else if(arrayIncludes('6O','4O','2O')){drawWinLine(100,508,510,90);}
+else if(arrayIncludes('0O','4O','8O')){drawWinLine(100,100,520,520);}
 else if(selectedSquares.length>=9){
     audio('./media/tie.mp3');
-    setTimeout(() => {
+    setTimeout(function() {
         resetGame();
     }, 1000);
 }
@@ -75,7 +75,7 @@ if(a===true && b===true && c===true){
 
 function disableClick(){
     body.style.pointerEvents='none';
-    setTimeout(() => {
+    setTimeout(function() {
         body.style.pointerEvents='auto';
     }, 1000);
 }
